f8e75e051c4d666c7560fb86d3155c63592da95b
DemExt <xldemlx@gmail.com>
2026-03-10 11:43:24 +0300

    fix13

--- page_analyzer/url_normalizer.py
+++ page_analyzer/url_normalizer.py
@@ -1,11 +1,7 @@
-import re
 from urllib.parse import urlparse
 
-def normalize_url(url):
-    # Добавляем протокол, если пользователь его забыл
-    if not re.match(r'^[a-zA-Z][a-zA-Z0-9+.-]*://', url):
-        url = f'http://{url}'
-        
-    parsed = urlparse(url)
-    # Оставляем ТОЛЬКО схему и домен в нижнем регистре
-    return f"{parsed.scheme.lower()}://{parsed.netloc.lower()}"
+def normalize_url(url: str) -> str:
+    parsed_url = urlparse(url)
+    normalized_url = f"{parsed_url.scheme}://{parsed_url.hostname}"
+    return normalized_url
+
